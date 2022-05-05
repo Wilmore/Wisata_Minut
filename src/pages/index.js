4969832e706db9fd4d18d22be578b6d2225fb9b7
perkinstangka <perkinstangk5@gmail.com>
2022-05-05 15:32:29 +0800

    menambahkan SplashScreen

---
+++ src/pages/index.js
@@ -0,0 +1,22 @@
+/* eslint-disable prettier/prettier */
+import SplashScreen from './SplashScreen';
+import SignIn from './SignIn';
+import SignUp from './SignUp';
+import Home from './Home';
+import OnBoard from './OnBoard';
+import RaewayaView from './RaewayaView';
+import RaewayaAkomodasi from './RaewayaAkomodasi';
+import DefaultView from './DefaultView';
+import DefaultAkomodasi from './DefaultAkomodasi';
+
+export {
+    SplashScreen,
+    SignIn,
+    SignUp,
+    Home,
+    OnBoard,
+    RaewayaView,
+    RaewayaAkomodasi,
+    DefaultView,
+    DefaultAkomodasi,
+};
