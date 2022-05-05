74b64fa97dfe5f11cb9269e18f887dea3049dee9
perkinstangka <perkinstangk5@gmail.com>
2022-05-05 15:23:01 +0800

    bundle

---
+++ metro.config.js
@@ -0,0 +1,20 @@
+/* eslint-disable eol-last */
+/* eslint-disable comma-dangle */
+/* eslint-disable quotes */
+/* eslint-disable prettier/prettier */
+const { getDefaultConfig } = require("metro-config");
+
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts }
+  } = await getDefaultConfig();
+  return {
+    transformer: {
+      babelTransformerPath: require.resolve("react-native-svg-transformer")
+    },
+    resolver: {
+      assetExts: assetExts.filter(ext => ext !== "svg"),
+      sourceExts: [...sourceExts, "svg"]
+    }
+  };
+})();
