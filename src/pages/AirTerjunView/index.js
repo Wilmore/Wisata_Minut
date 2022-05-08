3e331930ca59febcf1dacffe4bd8507e1ced2b2b
richardsangari <s21910051@student.unklab.ac.id>
2022-05-08 21:30:18 +0800

    Menabakan Air Terjun View di folder pages

---
+++ src/pages/AirTerjunView/index.js
@@ -0,0 +1,125 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable prettier/prettier */
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable prettier/prettier */
+/* eslint-disable semi */
+/* eslint-disable prettier/prettier */
+/* eslint-disable jsx-quotes */
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-unused-vars */
+/* eslint-disable prettier/prettier */
+import {ImageBackground, ScrollView, StyleSheet, Text, View, Image, Linking, TouchableOpacity} from 'react-native';
+import React from 'react';
+import { HomeButton, RaewayaHeader as Rah,
+         airTerjun as AT,        
+} from '../../assets';
+import { Button, Gap } from '../../components';
+
+const AirTerjunView = ({navigation}) => {
+  return (
+    <ScrollView style={styles.container}>
+      <Image source={AT} resizeMode='cover'/>
+      <Text style={styles.Text1}>Air Terjun Tunan</Text>
+
+         <View style={styles.btn}>
+          <Button title="Akomodasi" style={{padding: 50}} onPress={()=>navigation.navigate('DefaultAkomodasi')}/>
+          <Button title="Lokasi" onPress={ ()=>{ Linking.openURL('https://www.google.co.id/maps/place/Tunan+Waterfall/@1.5652216,124.9706939,16z/data=!4m9!1m2!2m1!1sAir+Terjun+Tunan!3m5!1s0x3287a69fd49a9587:0xea37bfe4033f8409!8m2!3d1.5689012!4d124.9784021!15sChBBaXIgVGVyanVuIFR1bmFukgESdG91cmlzdF9hdHRyYWN0aW9u')}} />
+        </View>
+      <View style={styles.Wrapper1}>
+        <Gap height={20} />
+        <Text style={styles.TextNotes}>
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est."
+        </Text>
+      </View>
+
+      <Text style={styles.texttitle1}>Harga Parkir & Masuk</Text>
+      <View style={styles.Wrapper2}>
+        <Text>Per/Orang</Text>
+        <View style={styles.backround}>
+          <Text style={styles.textprice}>2.500</Text>
+        </View>
+      </View>
+      <View style={styles.homepress}>
+          <TouchableOpacity activeOpacity={0.4} onPress={()=>navigation.navigate('Home')}>
+            <HomeButton/>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.Wrapper2}>
+        <Text>Per/Motor</Text>
+        <View style={styles.backround}>
+          <Text style={styles.textprice}>5.000</Text>
+        </View>
+      </View>
+
+      <View style={styles.Wrapper2}>
+        <Text>Per/Mobil</Text>
+        <View style={styles.backround}>
+          <Text style={styles.textprice}>10.000</Text>
+        </View>
+      </View>
+
+
+    </ScrollView>
+  );
+};
+
+export default AirTerjunView;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  Text1: {
+    fontFamily: 'Poppins-Medium',
+    fontSize: 30,
+    color: 'black',
+    padding: 15,
+  },
+  TextNotes: {
+    fontFamily:'Poppins-Regular',
+    color: 'black',
+    textAlign: 'center',
+  },
+  Wrapper1: {
+    paddingTop: 10,
+    paddingHorizontal: 40,
+  },
+  Wrapper2:{
+    flexDirection: 'row',
+    paddingHorizontal: 30,
+    paddingTop: 1,
+    alignItems: 'center',
+  },
+  backround:{
+    height: 40,
+    width: 90,
+    backgroundColor: '#65FA31',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 20,
+    marginLeft: 40,
+  },
+  textprice: {
+    color : 'black',
+    fontFamily: 'Poppins-Medium',
+    fontSize: 14,
+  },
+  texttitle1: {
+    fontFamily: 'Poppins-Bold',
+    fontSize: 25,
+    marginLeft: 20,
+    marginTop: 10,
+  },
+  btn: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  homepress:{
+    position: 'absolute',
+    bottom: 2,
+    left: 350,
+  },
+});
